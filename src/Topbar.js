5c03c6c903aeed09a1a79ed1f7f554daa8ee8dcb
Ricardo <568082704@qq.com>
2019-10-15 16:19:55 +0800

    save

--- src/Topbar.js
+++ src/Topbar.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import styled from 'styled-components'
 
-
+function Topbar(props) {
 const Title = styled.div`
   font-size:50px;
   color:green;
   border:1px solid red;
 `
+const Logo = styled.div`
+  width:40px;
+  height:40px;
+  background:grey;
 
-function Topbar() {
+`
+const Nav = styled.nav `
+  height:30px;
+  background:#333;
+  width:200px;
+  color:white;
+  margin-left:10px;
+`
+const Wrapper = styled.div`
+  display:flex;
+  align-items:center;
+  border:1px solid black;
+  padding:10px;
+`
+const onClick = ()=>{
+  console.log(1)
+}
   return(
-    <div>
-      <Title>topbar</Title>
-      其他
-    </div>
+    <Wrapper>
+      <Logo>logo</Logo>
+      <Nav onClick={onClick}>nav</Nav>
+    </Wrapper>
+    
   )
 }
 
